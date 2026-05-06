dcccbb92926620dce3adae011417acdbeae1d442
hiranya476 <dhaarmikreddy09@gmail.com>
2026-05-07 00:27:45 +0530

    Delete First Node (Singly Linked List).cpp

---
+++ Delete First Node (Singly Linked List).cpp
@@ -0,0 +1,62 @@
+// Write your code here
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node * next;
+    Node(int value){
+        data=value;
+        next=NULL;
+    }
+};
+class Linkedlist{
+    Node *head;
+    public:
+    Linkedlist(){
+        head=NULL;
+    }
+    void insertatend(int value){
+        Node *newnode=new Node(value);
+        if(head==NULL){
+            head=newnode;
+            return;
+        }
+        Node *temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newnode;
+    }
+    void display(){
+        Node *temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+    void deletefrombeginning(){
+        if(head==NULL){
+            cout<<"List is empty"<<endl;
+            return;
+        }
+        Node *temp=head;
+        head=head->next;
+        delete temp; 
+    }
+
+};
+int main(){
+    int n;
+    cin>>n;
+    int value;
+    Linkedlist list;
+    for(int i=0;i<n;i++){
+        cin>>value;
+        list.insertatend(value);
+    }
+    list.deletefrombeginning();
+    list.display();
+
+    return 0;
+}
