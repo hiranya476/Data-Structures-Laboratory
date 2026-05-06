47ac20815ae00fb11264725317293bf232b99fc5
hiranya476 <dhaarmikreddy09@gmail.com>
2026-05-07 00:33:57 +0530

    Insert at End (Doubly Linked List)

---
+++ Insert at End (Doubly Linked List).cpp
@@ -0,0 +1,55 @@
+// Write your code here
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int val){
+        data=val;
+        next=NULL;
+        prev=NULL;
+    }
+};
+class Doublylinkedlist{
+    public:
+    Node *head;
+    Doublylinkedlist(){
+        head=NULL;
+    }
+    void insertend(int val){
+        Node *newnode=new Node(val);
+        if(head==NULL){
+        head=newnode;
+        return;
+        }
+    Node *temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=newnode;
+    newnode->prev=temp;
+    }
+    void display(){
+        Node *temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+};
+int main(){
+    int n,m;
+    cin>>n;
+    Doublylinkedlist dlst;
+    for(int i=0;i<n;i++){
+        cin>>m;
+        dlst.insertend(m);
+    }
+    int val;
+    cin>>val;
+    dlst.insertend(val);
+    dlst.display();
+    return 0;
+}
