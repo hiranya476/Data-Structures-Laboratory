4879d8dff7553c783cf12b20e48791b2e0f918e5
hiranya476 <dhaarmikreddy09@gmail.com>
2026-05-07 00:38:35 +0530

    Delete Last Node (Doubly Linked List)

---
+++ Delete Last Node (Doubly Linked List).cpp
@@ -0,0 +1,73 @@
+// Write your code here
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node *next;
+    Node *prev;
+    Node(int val){
+        data=val;
+        next=NULL;
+        prev=NULL;
+    }
+};
+class Doublylinkedlist{
+    public:
+    Node *head;
+    Doublylinkedlist(){
+        head=NULL;
+    }
+    void insertend(int val){
+        Node *newnode=new Node(val);
+        if(head==NULL){
+        head=newnode;
+        return;
+        }
+    Node *temp=head;
+    while(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=newnode;
+    newnode->prev=temp;
+    }
+    void deletelast(){
+        if(head==NULL){
+            return;
+        }
+        if(head->next==NULL){
+            delete head;
+            head=NULL;
+            return;
+        }
+        Node *temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        Node *last=temp;
+        Node *prevnode=last->prev;
+        prevnode->next=NULL;
+        delete last;
+    }
+    void display(){
+        Node *temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+};
+int main(){
+    int n,m;
+    cin>>n;
+    Doublylinkedlist dlst;
+    for(int i=0;i<n;i++){
+        cin>>m;
+        dlst.insertend(m);
+    }
+    int val,pos;
+    cin>>val>>pos;
+    dlst.deletelast();
+    dlst.display();
+    return 0;
+}
