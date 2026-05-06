2744f59d454e90dd902b47d1ec47e75b54e3e476
hiranya476 <dhaarmikreddy09@gmail.com>
2026-05-07 00:59:05 +0530

    Insert at End (Circular Linked List)

---
+++ Insert at End (Circular Linked List).cpp
@@ -0,0 +1,56 @@
+// Write your code here
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node*next;
+    Node(int val){
+        data=val;
+        next=NULL;
+    }
+};
+class Crclrlst{
+    public:
+    Node* head;
+    Crclrlst(){
+        head=NULL;
+    }
+    void insertend(int val){
+        Node *newnode=new Node(val);
+        if(head==NULL){
+            head=newnode;
+            newnode->next=head;
+            return;
+        }
+        Node *temp=head;
+        while(temp->next!=head){
+            temp=temp->next;
+        }
+        temp->next=newnode;
+        newnode->next=head;
+    }
+    void display(){
+        if(head==NULL){
+            return;
+        }
+        Node* temp=head;
+        do{
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }while(temp!=head);
+    }
+};
+int main(){
+    int n,m;
+    cin>>n;
+    Crclrlst clst;
+    for(int i=0;i<n;i++){
+        cin>>m;
+        clst.insertend(m);
+    }
+    cin>>m;
+    clst.insertend(m);
+    clst.display();
+    return 0;
+}
