4ce6dc46eacd2831a7b0904b0af115c68e82ff03
hiranya476 <dhaarmikreddy09@gmail.com>
2026-05-07 00:25:53 +0530

    Insert at End (Singly Linked List).cpp

---
+++ Insert at End (Singly Linked List).cpp
@@ -0,0 +1,54 @@
+// Write your code here
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node * next;
+    Node(int value){
+        data=value;
+        next=NULL;
+    }
+};
+class Linkedlist{
+    Node *head;
+    public:
+    Linkedlist(){
+        head=NULL;
+    }
+    void display(){
+        Node *temp=head;
+        while(temp!=NULL){
+            cout<<temp->data<<" ";
+            temp=temp->next;
+        }
+    }
+    void insertatend(int value){
+        Node *newnode=new Node(value);
+        if(head==NULL){
+            head=newnode;
+            return;
+        }
+        Node *temp=head;
+        while(temp->next!=NULL){
+            temp=temp->next;
+        }
+        temp->next=newnode;
+    }
+
+};
+int main(){
+    int n;
+    cin>>n;
+    int value;
+    Linkedlist list;
+    for(int i=0;i<n;i++){
+        cin>>value;
+        list.insertatend(value);
+    }
+    cin>>value;
+    list.insertatend(value);
+    list.display();
+
+    return 0;
+}
